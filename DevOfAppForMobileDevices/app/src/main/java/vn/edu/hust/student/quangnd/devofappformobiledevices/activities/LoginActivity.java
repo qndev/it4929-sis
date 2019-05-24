c3d98b3be2ab5830ba9f5ac5f0c496f5caff61e6
Dinh Quang <quangnd.hust@gmail.com>
2019-05-24 17:27:04 +0700

    Add login, signup layout

---
+++ DevOfAppForMobileDevices/app/src/main/java/vn/edu/hust/student/quangnd/devofappformobiledevices/activities/LoginActivity.java
@@ -0,0 +1,41 @@
+package vn.edu.hust.student.quangnd.devofappformobiledevices.activities;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v4.view.LayoutInflaterCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.mikepenz.iconics.context.IconicsLayoutInflater2;
+import vn.edu.hust.student.quangnd.devofappformobiledevices.databinding.ActivityLoginBinding;
+import vn.edu.hust.student.quangnd.devofappformobiledevices.R;
+
+public class LoginActivity extends AppCompatActivity {
+    ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        LayoutInflaterCompat.setFactory2(getLayoutInflater(), new IconicsLayoutInflater2(getDelegate()));
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+
+
+    }
+
+    public void signup(View view) {
+        startActivity(new Intent(this, SignupActivity.class));
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return true;
+    }
+}
---
+++ DevOfAppForMobileDevices/app/src/main/java/vn/edu/hust/student/quangnd/devofappformobiledevices/activities/SignupActivity.java
@@ -0,0 +1,39 @@
+package vn.edu.hust.student.quangnd.devofappformobiledevices.activities;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v4.view.LayoutInflaterCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.mikepenz.iconics.context.IconicsLayoutInflater2;
+import vn.edu.hust.student.quangnd.devofappformobiledevices.databinding.ActivitySignupBinding;
+import vn.edu.hust.student.quangnd.devofappformobiledevices.R;
+
+public class SignupActivity extends AppCompatActivity {
+    ActivitySignupBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        LayoutInflaterCompat.setFactory2(getLayoutInflater(), new IconicsLayoutInflater2(getDelegate()));
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_signup);
+    }
+
+
+    public void login(View view) {
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return true;
+    }
+}
